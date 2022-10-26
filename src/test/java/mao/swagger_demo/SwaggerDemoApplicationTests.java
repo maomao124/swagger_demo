3803b27565246a2281d72829c856cf68db070252
mao <1296193245@qq.com>
2022-10-26 20:41:06 +0800

    swagger_demo

---
+++ src/test/java/mao/swagger_demo/SwaggerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.swagger_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
